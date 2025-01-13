b0d2d47881101818da1324929fd6ac0efbd6475c
Pawat Yamwong <tancoding2023@gmail.com>
2025-01-13 11:45:25 +0700

    update picture component

--- src/app/patient-table/patient-table.component.ts
+++ src/app/patient-table/patient-table.component.ts
@@ -1,12 +1,123 @@
-import { Component } from '@angular/core';
-
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { DataTableDirective, DataTablesModule } from 'angular-datatables';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { PatientService } from '../service/patient.service';
+import { Config } from 'datatables.net';
+import { Subject } from 'rxjs';
+import { PictureComponent } from '../picture/picture.component';
 @Component({
   selector: 'app-patient-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    DataTablesModule,
+    MatIconModule,
+    MatProgressBarModule,
+    NgFor,
+    NgIf,
+  ],
   templateUrl: './patient-table.component.html',
   styleUrl: './patient-table.component.scss'
 })
-export class PatientTableComponent {
+export class PatientTableComponent implements AfterViewInit, OnDestroy, OnInit{
+  constructor
+    (
+      private _patientService: PatientService,
+      private _changeDetectorRef: ChangeDetectorRef,
+      private dialog: MatDialog,
+    ) { }
 
+    dtOptions: Config = {};
+    dtTrigger: Subject<any> = new Subject();
+    @ViewChild(DataTableDirective)
+    dtElement!: DataTableDirective
+    languageUrl = 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/th.json';
+    dataRow: any[] = [];
+    isLoading: boolean = false;
+
+    ngOnInit() {
+      this.loadTable();
+    }
+
+    showPicture(imgObject: any): void {
+      this.dialog
+        .open(PictureComponent, {
+          width: 'auto',
+          height: 'auto',
+          disableClose: true,
+          data: {
+            imgSelected: imgObject,
+          },
+          panelClass: 'picture-dialog',
+        })
+        .afterClosed()
+        .subscribe(() => {
+
+        });
+    }
+
+    pages = { current_page: 1, last_page: 1, per_page: 10, begin: 0 };
+
+    loadTable(): void {
+  
+      const that = this;
+      this.dtOptions = {
+        pagingType: 'full_numbers',
+        pageLength: 10,
+        serverSide: true,
+        processing: true,
+        language: {
+          url: this.languageUrl,
+        },
+        ajax: (dataTablesParameters: any, callback) => {
+          that._patientService
+            .getPage(dataTablesParameters)
+            .subscribe((resp: any) => {
+              console.log(resp);
+              
+              this.dataRow = resp.data;
+              this.pages.current_page = resp.meta.currentPage;
+              this.pages.last_page = resp.meta.totalPages;
+              this.pages.per_page = resp.meta.itemsPerPage;
+              if (resp.meta.currentPage > 1) {
+                this.pages.begin =
+                resp.meta.itemsPerPage * resp.meta.currentPage - 1;
+              } else {
+                this.pages.begin = 0;
+              }
+  
+              callback({
+                recordsTotal: resp.meta.totalItems,
+                recordsFiltered: resp.meta.totalItems,
+                data: [],
+              });
+              this._changeDetectorRef.markForCheck();
+            });
+        }
+      };
+  
+    }
+  
+    ngAfterViewInit(): void {
+      this.dtTrigger.next(null);
+    }
+  
+    ngOnDestroy(): void {
+      // Do not forget to unsubscribe the event
+      this.dtTrigger.unsubscribe();
+    }
+  
+    rerender(): void {
+      if (this.dtElement.dtInstance) {
+        this.dtElement.dtInstance.then((dtInstance) => {
+          dtInstance.ajax.reload();
+        });
+      } else {
+        // If DataTables instance is not ready, reinitialize
+        this.dtTrigger.next(null);
+      }
+    }
 }
---
+++ src/app/picture/picture.component.ts
@@ -0,0 +1,63 @@
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Subject } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
+@Component({
+  selector: 'app-picture',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  templateUrl: './picture.component.html',
+  styleUrl: './picture.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PictureComponent implements OnInit, OnDestroy {
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
+  imgSelected: string | undefined;
+  loading: boolean = true;
+  error: boolean = false;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private _data: any,
+    private _dialogRef: MatDialogRef<PictureComponent>,
+  ) {
+
+  }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  ngOnInit(): void {
+    this.imgSelected = environment.baseURL + this._data.imgSelected;
+
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next(null);
+    this._unsubscribeAll.complete();
+  }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Public methods
+  // -----------------------------------------------------------------------------------------------------
+
+  onImageLoad(): void {
+    this.loading = false;
+    this.error = false;
+  }
+
+  onImageError(): void {
+    this.loading = false;
+    this.error = true;
+  }
+
+  closeDialog(): void {
+    this._dialogRef.close();
+  }
+
+}
